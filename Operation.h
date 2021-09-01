f3b9ecb249a34e1f722db164dfe6d96806ad3cd8
Boris Nikiforov <nikiforov5000@gmail.com>
2021-09-01 15:44:26 +0300

    move members in private

--- Operation.h
+++ Operation.h
@@ -3,10 +3,10 @@
 #include "Token.h"
 
 class Operation {
-public:
 	Value m_val1;
 	Value m_val2;
 	Token m_oper;
+public:
 	Operation(const Value& v1, const Value& v2, Token op) :m_val1(v1), m_val2(v2), m_oper(op){
 		//std::cout << "Operation(const Value& v1, const Value& v2, Token op). count:" << ++constrCount << std::endl;
 	}
